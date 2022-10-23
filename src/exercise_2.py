d9b0a930fcfbc14d4c9b3d87c9e9fa8128e5f818
alexVera99 <alexanderjoel.vera99@gmail.com>
2022-10-23 17:17:02 +0200

    add: exercise 2 done

---
+++ src/exercise_2.py
@@ -0,0 +1,52 @@
+"""Solution for Exercise 2."""
+import pathlib
+import exercise_3 as ex_3
+import utils as ut
+
+
+def create_video_with_yuv_histogram(filename_path: pathlib.Path,
+                                    output_filename: str = ""):
+    """
+    Create a video with the YUV histogram overlaid.
+
+    :param filename_path: video filename path
+    :param output_filename: output filename
+    :return: no return
+    """
+    if output_filename == "":
+        video_name = filename_path.name.split(".")[0]
+        output_filename = f"{video_name}_with_hist"
+
+    output_filename_path = ut.rename_from_path(filename_path, output_filename)
+
+    # Computing histogram size
+    w_video, h_video = ex_3.get_video_dims(filename_path)
+    w_hist = w_video // 4
+    h_video = h_video // 2
+
+    cmd = ["ffmpeg", "-y", "-i", filename_path,
+           "-vf",
+           f"split=2[a][b],"
+           f"[b]histogram,format=yuva444p,scale={w_hist}:{h_video}[hh],"
+           f"[a][hh]overlay",
+           output_filename_path]
+
+    _, stderr = ut.exec_in_shell_wrapper(cmd)
+
+    ut.check_shell_stderr(stderr,
+                          f"Could not get the YUV histogram {filename_path}")
+
+
+def main():
+    """
+    Test the above functions.
+
+    :return no return
+    """
+    video_filename = pathlib.Path("../data/bbb.mp4")
+
+    create_video_with_yuv_histogram(video_filename)
+
+
+if __name__ == "__main__":
+    main()
